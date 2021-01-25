3eef17f0c793aa64d41c65b8849015d74256342d
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2021-01-25 20:34:56 -0300

    Directory structure

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import db from "../db.json";
+import Widget from '../src/components/Widget'
 
 const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -20,55 +21,6 @@ export const QuizContainer = styled.div`
   }
 `;
 
-const Widget = styled.div`
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
-  background-color: #1c1814;
-  border-radius: 4px;
-  overflow: hidden;
-
-  h1,
-  h2,
-  h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
-`;
-
-Widget.Header = styled.header`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 18px 32px;
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  * {
-    margin: 0;
-  }
-`;
-
-Widget.Content = styled.div`
-  padding: 24px 32px 32px 32px;
-  & > *:first-child {
-    margin-top: 0;
-  }
-  & > *:last-child {
-    margin-bottom: 0;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-`;
-
 export default function Home() {
   return (
     <BackgroundImage>
@@ -83,9 +35,6 @@ export default function Home() {
         </Widget>
 
         <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
           </Widget.Content>
