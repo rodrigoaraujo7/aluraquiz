b37920d45327abaaad56f8bc96a32626364e6a80
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2021-01-25 20:06:50 -0300

    Quiz Container

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,23 @@ const BackgroundImage = styled.div`
   min-height: 100vh;
 `;
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
   return (
     <BackgroundImage>
+      <QuizContainer>
+        dwadwa
+      </QuizContainer>
     </BackgroundImage>
   );
 }
