11bfaea5f6059f9c2c619eaac54904a48fd58341
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2021-01-31 21:05:25 -0300

    Loading Screen, Quiz Screen & Congratulations Screen

--- pages/quiz.js
+++ pages/quiz.js
@@ -6,7 +6,7 @@ import Widget from "../src/components/Widget";
 import Input from "../src/components/Input";
 import GithubCorner from "../src/components/GithubCorner";
 
-const QuestionWidget = ({ questionsIndex, totalQuestions, questions }) => {
+const QuestionWidget = ({ questionsIndex, totalQuestions, questions, onSubmit }) => {
   const questionId = `question__${questionsIndex}`;
 
   return (
@@ -26,7 +26,10 @@ const QuestionWidget = ({ questionsIndex, totalQuestions, questions }) => {
           <Widget.Content>
             <h1>{questions.title}</h1>
             <p>{questions.description}</p>
-            <form>
+            <form onSubmit={(infosDoEvento) => {
+              infosDoEvento.preventDefault();
+              onSubmit();
+            }}>
               {questions.alternatives.map((alternative, alternativeIndex) => {
                 const alternativeId = `alternative__${alternativeIndex}`;
                 return (
@@ -36,8 +39,9 @@ const QuestionWidget = ({ questionsIndex, totalQuestions, questions }) => {
                   </Widget.Topic>
                 );
               })}
+
+              <Input.Button type="submit">CONFIRMAR</Input.Button>
             </form>
-            <Input.Button type="submit">CONFIRMAR</Input.Button>
           </Widget.Content>
         </Widget>
   );
@@ -52,7 +56,8 @@ const screenStates = {
 export default function QuizPage() {
     const [screenState, setScreenState] = React.useState(screenStates.LOADING);
     const totalQuestions = db.questions.length;
-    const questionsIndex = 0;
+    const [currentQuestion, setcurrentQuestion] = React.useState(0);
+    const questionsIndex = currentQuestion;
     const questions = db.questions[questionsIndex];
     
     React.useEffect(() => {
@@ -60,6 +65,15 @@ export default function QuizPage() {
         setScreenState(screenStates.QUIZ);
       }, 1 * 1000); 
     }, []);
+
+    function handleSubmit() {
+      const nextQuestion = questionsIndex + 1;
+      if (nextQuestion < totalQuestions) {
+        setcurrentQuestion(questionsIndex + 1);
+      } else {
+        setScreenState(screenStates.RESULT)
+      }
+    }
 
     return (
         <QuizBackground backgroundImage={db.bg}>
@@ -69,6 +83,7 @@ export default function QuizPage() {
                   questionsIndex={questionsIndex}
                   totalQuestions={totalQuestions}
                   questions={questions}
+                  onSubmit={handleSubmit}
               />
             )}
 
@@ -80,7 +95,12 @@ export default function QuizPage() {
             }}> 
               <Widget.Loading />   
             </Widget.Content>}
-            {screenState === screenStates.RESULT && <div>You got X questions right, congratualions!!!</div>}
+            {screenState === screenStates.RESULT && <Widget.Content style={{ height: '350px' }}>
+              <center> 
+                <h1>Congratulations!!!</h1>
+                <h3>You got X questions right</h3>
+              </center>
+            </Widget.Content>}
         </QuizContainer>
 
          <GithubCorner projectUrl="https://github.com/rodrigoaraujo7" />
